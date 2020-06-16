04adb9c47fd63dfca0a865714b41f5520e6d37c3
Joe Izzard <Joe@Izzard.me>
2020-06-16 09:26:19 +0100

    feat: better usage of variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 WIKI="https://github.com/${GITHUB_REPOSITORY}.wiki.git"
-WIKIP="https://${USERNAME}:${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.wiki.git"
+WIKIP="https://${INPUT_USERNAME}:${INPUT_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.wiki.git"
 
 echo "Cloning WIKI Repo..."
 git clone $WIKI /wiki
@@ -15,18 +15,15 @@ cd /github/workspace
 cp -a wiki/. /wiki
 
 echo "Git Config..."
-git config --global user.email "${USERNAME}@user.noreply.github.com"
-git config --global user.name "${USERNAME}"
+echo "-> User: ${INPUT_COMMIT_USERNAME}"
+echo "-> Email: ${INPUT_COMMIT_EMAIL}"
+git config --global user.email "${INPUT_COMMIT_EMAIL}"
+git config --global user.name "${INPUT_COMMIT_USERNAME}"
 
 echo "Commit..."
-if [ -z ${INPUT_COMMIT_MESSAGE} ]; then
-    echo "Commit Message not set"
-else
-    echo ${INPUT_COMMIT_MESSAGE}
-fi
 cd /wiki
 git add -A
-git commit -m "Wiki Sync"
+git commit -m "${INPUT_COMMIT_MESSAGE}"
 git push $WIKIP
 
 echo "Finished!"
